e5fae22db941dd6e920034b611ba8bf99103ff20
smarten <user@example.com>
2023-04-30 15:39:56 +0200

    Add a sample markdown rendered page through mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: {
+				posts: 'src/routes/posts/md-layout.svelte'
+			}
+		}),
+	],
 	kit: {
 		adapter: adapter()
-	}
+	},
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
