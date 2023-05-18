51cd04cbaa9f853e2bc841ced83c159deca3f2b7
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-05-18 23:11:30 +0200

    Use Iconify for applying icons

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import IconDotsHorizontal from 'virtual:icons/mdi/dots-horizontal'
+
   const anchorLinks = [
     { title: 'Home',        href: '/'            },
     { title: 'Evenementen', href: '/evenementen' },
@@ -8,31 +10,31 @@
   ]
 </script>
 
-<header class="header bg-secondary items-center min-h-[15vh]">
-  <h1 class="logo text-primary text-lg">
-    <a href="/">ATTV'71</a>
-  </h1>
-  <nav class="main-nav">
-    <ul class="flex gap-6">
-      {#each anchorLinks as link}
-        <li><a class="text-primary text-lg no-underline" href={link.href}>{link.title}</a></li>
-      {/each}
-    </ul>
-  </nav>
+<header class="bg-secondary text-primary">
+  <div class="mx-auto flex justify-between py-12 px-14 max-w-[105rem]">
+    <h1 class="text-2xl flex items-center">
+      <a href="/">ATTV'71</a>
+    </h1>
+    <div class="flex items-center">
+      <nav>
+        <ul class="flex gap-6 text-xl">
+          {#each anchorLinks as link}
+            <li><a class="hover:underline" href={link.href}>{link.title}</a></li>
+          {/each}
+        </ul>
+      </nav>
+      <div class="relative px-10 mx-10 border-l border-neutral-900">
+        <button class="nav-toggle-button">
+          <IconDotsHorizontal class="inline" height="40" width="40" />
+          <span class="absolute left-0 right-0 top-10 text-xs">Menu</span>     
+        </button>
+      </div>
+    </div>
+  </div>
 </header>
 
 <style>
-  .header {
-    display: grid;
-    grid-template-areas: ". logo nav-links";
-    grid-template-columns: 9% 49% 1fr;
-  }
-
-  .logo {
-    grid-area: logo
-  }
-
-  .main-nav {
-    grid-area: nav-links
+  .nav-toggle-button:hover > span {
+    text-decoration: underline;
   }
 </style>
