1e76bc5e03c649cc40db1869aa8194434feec4f9
smarten <user@example.com>
2023-05-29 14:29:39 +0200

    Add page for currently sitting bestuur

---
+++ src/routes/bestuur/+page.svelte
@@ -0,0 +1,29 @@
+<div class="container mx-auto">
+  <h2 class="text-5xl font-bold tracking-tight">Bestuur</h2>
+  <div class="mt-11 flex flex-col gap-10">
+    <div class=text-lg>
+      <h3 class="text-2xl font-bold tracking-tight">Voorzitter</h3>
+      <p>Peter Verweijen</p>
+    </div>
+    <div>
+      <h3 class="text-2xl font-bold tracking-tight">Secretaris</h3>
+      <p>Inge Leenen</p>
+    </div>
+    <div>
+      <h3 class="text-2xl font-bold tracking-tight">Penningmeester</h3>
+      <p>Mieke Classens</p>
+    </div>
+    <div>
+      <h3 class="text-2xl font-bold tracking-tight">Wedstrijdsecretaris</h3>
+      <p>Erik van Loon</p>
+    </div>
+    <div>
+      <h3 class="text-2xl font-bold tracking-tight">PR & Algemene Zaken</h3>
+      <p>Niels van Heugten</p>
+    </div>
+    <div>
+      <h3 class="text-2xl font-bold tracking-tight">Sponsoring & Algemene zaken</h3>
+      <p>Toon van Loon</p>
+    </div>
+  </div>
+</div>
