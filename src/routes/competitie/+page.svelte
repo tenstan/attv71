c3fb561fe9a546e9b68dbeba16c6fe64f4f586fd
smarten <user@example.com>
2023-05-29 14:27:22 +0200

    Add page for explaining competitie

---
+++ src/routes/competitie/+page.svelte
@@ -0,0 +1,18 @@
+<div class="container mx-auto">
+  <h2 class="text-5xl font-bold tracking-tight mb-8">Competitie</h2>
+  <div class="text-lg">
+    <p>
+      De NTTB organiseert tweemaal per jaar de afdelingscompetitie ZuidWest, waar ATTV'71 aan deelneemt. 
+      Deze competitie is van januari t/m april en van september t/m november. 
+      Ieder seizoen bestaat normaal gesproken uit 10 wedstrijddagen, waarbij ieder team 3 spelers meeneemt. 
+      Iedere speler van het ene team speelt tegen iedere speler van het andere team, in totaal 9 wedstrijden. 
+      Er wordt ook eenmaal een dubbelspel gespeeld, waarbij twee spelers van het ene team het opnemen tegen twee spelers uit het andere team. 
+      In totaal worden er dus 10 wedstrijden gespeeld op een wedstrijddag.
+    </p>
+    <p class="mt-4">
+      Voor de overige regels is meer informatie op
+      <a class="text-secondary underline" href="https://zuidwest.nttb.nl/competitie/algemene-informatie/">de website van de NTTB</a>
+      te vinden.
+    </p>
+  </div>
+</div>
