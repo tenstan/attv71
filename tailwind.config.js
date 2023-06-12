76dc6d11ec433653549b1de633455be93ab74c36
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-06-13 01:16:10 +0200

    Make tailwind typography plugin for consistent with the rest of the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,30 @@ export default {
     extend: {
       colors: {
         primary: '#212120',
-        secondary: '#EDD100'
-      }
+        secondary: '#EDD100',
+        faded: '#8A8A88',
+      },
+      typography: (theme) => ({
+        article: {
+          css: {
+            '--tw-prose-invert-body': theme('colors.white'),
+            '--tw-prose-invert-headings': theme('colors.white'),
+            '--tw-prose-invert-links': theme('colors.secondary'),
+            '--tw-prose-invert-bold': theme('colors.white'),
+            '--tw-prose-invert-counters': theme('colors.white'),
+            '--tw-prose-invert-bullets': theme('colors.white'),
+            
+            img: {
+              'margin-left': 'auto',
+              'margin-right': 'auto',
+            }
+          }
+        }
+      })
     },
   },
   plugins: [
-    require('@tailwindcss/typography') // Used to render markdown with appropiate styling
+    require('@tailwindcss/typography') // Used to render markdown with appropriate styling
   ],
 }
 
