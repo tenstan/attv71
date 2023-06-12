c42df0c4bffbf52b7781b1f22ad10dff6b1b904c
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-06-13 01:20:26 +0200

    Define general layout of an article

---
+++ src/lib/components/PreviewArticle.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import IconCalendarBlankOutline from 'virtual:icons/mdi/calendar-blank-outline'
+    import ArticleContent from './ArticleContent.svelte';
+
+  export let href: string;
+  export let title: string;
+  export let date: Date;
+</script>
+
+<article class="max-w-6xl">
+  <div class="max-w-4xl mx-auto">
+    <h2 class="text-center text-6xl">
+      <a class="no-underline font-bold hover:underline" {href}>{title}</a>
+    </h2>
+  </div>
+  <div class="flex gap-2 items-center text-faded mx-auto max-w-fit mt-8">
+    <IconCalendarBlankOutline class="inline-block"/>
+    <p>{date.toLocaleDateString('nl-NL')}</p>
+  </div>
+  <div class="prose prose-article prose-invert mx-auto">
+    <slot />
+  </div>
+</article>
