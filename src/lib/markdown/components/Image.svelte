fc6dd45173dcc91d030478fa111058fb9c682d8e
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-10-29 20:32:13 +0100

    Load markdown into Article components

---
+++ src/lib/markdown/components/Image.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
+  export let alt: string = 'GENERIC';
+  export let src: string;
+</script>
+
+<div class:max-w-[15%]="{size === 'xs'}"
+     class:max-w-[30%]="{size === 'sm'}"
+     class:max-w-[45%]="{size === 'md'}"
+     class:max-w-[60%]="{size === 'lg'}"
+     class:max-w-[75%]="{size === 'xl'}">
+  <img {src} {alt} />
+</div>
