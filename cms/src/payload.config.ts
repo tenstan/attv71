f4aa1b4da736d007c693df857e207df2dfb02b78
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-11-30 23:24:59 +0100

    Specify database to use for CMS

--- cms/src/payload.config.ts
+++ cms/src/payload.config.ts
@@ -18,5 +18,8 @@ export default buildConfig({
   plugins: [payloadCloud()],
   db: mongooseAdapter({
     url: process.env.MONGODB_CONNECTION_STRING,
+    connectOptions: {
+      dbName: process.env.MONGODB_DATABASE_NAME
+    }
   }),
 })
