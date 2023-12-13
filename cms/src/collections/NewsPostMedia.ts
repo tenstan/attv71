825c3d1bcd25a684f666fa4a32fc969b0b607d1b
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-12-14 00:03:40 +0100

    Remove disableLocalStorage option from the Media collection and set it on the azure blob storage adapter config instead

--- cms/src/collections/NewsPostMedia.ts
+++ cms/src/collections/NewsPostMedia.ts
@@ -1,14 +1,11 @@
 import { CollectionConfig } from "payload/types";
 
-const isDevelopment = process.env.NODE_ENV === 'development'
-
 const NewsPostMedia: CollectionConfig = {
   slug: 'news-post-media',
   fields: [],
   upload: {
     staticURL: '/news-post-media',
     staticDir: '../media',
-    disableLocalStorage: !isDevelopment,
     mimeTypes: [ 'image/*' ]
   }
 }
