8fc3b0c86a5ebcfbe5b4434e5dc4476dce952504
Stan Martens <31350208+tenstan@users.noreply.github.com>
2023-12-23 22:38:23 +0100

    Implement workflow for deploying app with node adapter

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -2,8 +2,6 @@ import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
-
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
