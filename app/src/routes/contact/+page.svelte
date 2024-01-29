76cbfe8defbb5879775c6f5471dbe86c656807b0
Stan Martens <31350208+tenstan@users.noreply.github.com>
2024-01-29 15:35:52 +0100

    Make Contact page responsive

--- app/src/routes/contact/+page.svelte
+++ app/src/routes/contact/+page.svelte
@@ -1,41 +1,57 @@
 <script>
-  import IconWhatsapp from "virtual:icons/mdi/whatsapp";
-  import IconEmail from "virtual:icons/mdi/email";
-  import IconPhone from "virtual:icons/mdi/phone";
-  import IconInstagram from "virtual:icons/mdi/instagram";
+  import MapLocation from "./MapLocation.svelte";
+  import ContactDetails from "./ContactDetails.svelte";
+  import ContactIcons from "./ContactIcons.svelte";
+  import EntranceInfo from "./EntranceInfo.svelte";
 </script>
 
-<div class="container mx-auto">
-  <h2 class="text-5xl font-bold">Plaats</h2>
-  <div class="text-lg mt-11 grid gap-x-10 gap-y-8">
-    <iframe
-      title="Sporthal locatie"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2092.908568319332!2d5.752750288539847!3d51.40485975182589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c73ac4d61c40f5%3A0xe174996ec1ab2477!2sDe%20Schop!5e0!3m2!1sen!2snl!4v1675379157059!5m2!1sen!2snl"
-      width="600"
-      height="450"
-      allow="fullscreen"
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-      class="row-span-3"
-    />
-    <div class="col-start-2 tracking-widest border border-stone-700 p-7 mb-auto">
-      <p class="font-bold">Sporthal De Schop</p>
-      <p>Lienderweg 66</p>
-      <p>5721 CN Asten</p>
-      <p>Iedere donderdag van 19:00 tot 23:00</p>
+<div class="container">
+  <h2 class="page-title">Plaats</h2>
+  <div class="contact-row">
+    <div class="min-map-size">
+      <MapLocation />
     </div>
-    <div class="col-start-2 flex justify-evenly text-secondary">
-      <!-- TODO: dynamic height? -->
-      <a href="https://wa.me/31636295860"><IconWhatsapp height="70" width="70" /></a>
-      <a href="mailto:attv71@hotmail.com"><IconEmail height="70" width="70" /></a>
-      <a href="tel:+31 6 3629586"><IconPhone height="70" width="70" /></a>
-      <a href="https://www.instagram.com/attv.asten/"><IconInstagram height="70" width="70" /></a>
+    <div class="contact-info">
+      <ContactDetails />
+      <ContactIcons />
+      <EntranceInfo />
     </div>
-    <p class="col-start-2">
-      Parkeren kan vooraan het gebouw van De Schop. Bij binnenkomst van De
-      Schop rechtdoor lopen, daar hangt een krijtbord dat aangeeft welke
-      kleedlokalen beschikbaar zijn (ATTV'71 Mannen / Vrouwen). Aan de andere
-      zijde via de deur van het kleedlokaal bevindt zich de sporthal.
-    </p>
   </div>
 </div>
+
+<style>
+  .container {
+    padding-inline: 2rem;
+    max-width: 80rem;
+    margin: auto;
+  }
+
+  .page-title {
+    font-size: clamp(2rem, 4vw, 3rem);
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 1em;
+  }
+
+  .contact-row {
+    display: flex;
+    gap: 3rem;
+    flex-wrap: wrap;
+  }
+
+  .contact-row > * {
+    flex: 1;
+  }
+
+  .contact-info {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    font-size: 1.125rem;
+  }
+
+  .min-map-size {
+    min-height: 300px;
+    min-width: 240px;
+  }
+</style>
---
+++ app/src/routes/contact/MapLocation.svelte
@@ -0,0 +1,16 @@
+<iframe
+  title="Sporthal locatie"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2092.908568319332!2d5.752750288539847!3d51.40485975182589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c73ac4d61c40f5%3A0xe174996ec1ab2477!2sDe%20Schop!5e0!3m2!1sen!2snl!4v1675379157059!5m2!1sen!2snl"
+  allow="fullscreen"
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade"
+  class="google-maps"
+  style="border: 0;"
+/>
+
+<style>
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+</style>
