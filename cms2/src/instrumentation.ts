a99220facd0382494af9318664807c9890d57abb
Stan Martens <31350208+tenstan@users.noreply.github.com>
2025-02-01 18:43:31 +0100

    Only load app insights when in nodejs environment

--- cms2/src/instrumentation.ts
+++ cms2/src/instrumentation.ts
@@ -1,12 +1,16 @@
-import { AzureMonitorTraceExporter } from '@azure/monitor-opentelemetry-exporter'
+import type { SpanExporter } from '@opentelemetry/sdk-trace-base'
 import { registerOTel } from '@vercel/otel'
 
 // Thanks to https://github.com/Azure/static-web-apps/issues/1269#issuecomment-2002515095.
 export async function register() {
-  registerOTel({
-    serviceName: 'attv71',
-    traceExporter: new AzureMonitorTraceExporter({
+  let traceExporter: SpanExporter | undefined
+
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { AzureMonitorTraceExporter } = await import('@azure/monitor-opentelemetry-exporter')
+    traceExporter = new AzureMonitorTraceExporter({
       connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
-    }),
-  })
+    })
+  }
+
+  registerOTel({ serviceName: 'attv71', traceExporter })
 }
