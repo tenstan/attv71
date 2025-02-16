d99e8357f4fa226453b763214aa011d718490921
Stan Martens <31350208+tenstan@users.noreply.github.com>
2025-02-16 23:20:17 +0100

    Suppress warnings about unresolved modules during build

--- cms2/next.config.mjs
+++ cms2/next.config.mjs
@@ -2,7 +2,22 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // @azure/functions-core is used by Azure Application Insights.
+      // The module can only be resolved when hosting this application in Azure.
+      // The package is added to `externals` to suppress warnings during build.
+      config.externals.push('@azure/functions-core')
+
+      // @opentelemetry/exporter-jaeger seems to be a package that gets improperly resolved by Webpack.
+      // Instrumentation still functions as expected (as far as I can see).
+      // The package is added to `externals` to suppress warnings during build.
+      //
+      // Relevant GitHub issue: https://github.com/open-telemetry/opentelemetry-js/issues/4297
+      config.externals.push('@opentelemetry/exporter-jaeger')
+    }
+    return config
+  },
 }
 
 export default withPayload(nextConfig)
