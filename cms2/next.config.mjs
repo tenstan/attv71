1985b1df00d3d3b1805b3518e0d0b7b24acf300a
Stan Martens <31350208+tenstan@users.noreply.github.com>
2025-02-21 21:01:44 +0100

    Build nextjs as standalone to speed up the CI/CD process

--- cms2/next.config.mjs
+++ cms2/next.config.mjs
@@ -2,6 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: process.env.BUILD_MODE === 'standalone' ? 'standalone' : 'export',
   webpack: (config, { isServer }) => {
     if (isServer) {
       // @azure/functions-core is used by Azure Application Insights.
