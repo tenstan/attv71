4005653ff06806a13e0786d0724e7a8393ab4464
Stan Martens <31350208+tenstan@users.noreply.github.com>
2025-02-28 02:06:30 +0100

    Set admin role for first ever signed in user in CMS

--- cms2/src/collections/Users.ts
+++ cms2/src/collections/Users.ts
@@ -1,10 +1,39 @@
+import { User } from 'src/payload-types'
 import { createRoleField } from '../access/roles'
 import { isAdmin, isLoggedIn } from '../access/validation'
-import type { CollectionConfig } from 'payload'
+import type { CollectionBeforeChangeHook, CollectionConfig } from 'payload'
 
-export const Users: CollectionConfig = {
+const setAdminRoleForFirstEverUserHook: CollectionBeforeChangeHook<User> = async ({
+  req,
+  data,
+  operation,
+}) => {
+  if (operation !== 'create') {
+    return data
+  }
+
+  const existingUsers = await req.payload.find({
+    collection: Users.slug,
+    limit: 1,
+  })
+
+  if (existingUsers.totalDocs > 0) {
+    return data
+  }
+
+  if (!data.roles) {
+    data.roles = ['admin']
+  } else if (!data.roles.includes('admin')) {
+    data.roles.push('admin')
+  }
+
+  return data
+}
+
+export const Users: CollectionConfig & { slug: 'users' } = {
   slug: 'users',
   auth: {
+    // Auth is handled with Microsoft Entra ID (see src/oauth/entra-id.ts)
     disableLocalStrategy: true,
   },
   access: {
@@ -15,6 +44,9 @@ export const Users: CollectionConfig = {
   },
   admin: {
     useAsTitle: 'email',
+  },
+  hooks: {
+    beforeChange: [setAdminRoleForFirstEverUserHook],
   },
   fields: [createRoleField()],
 }
