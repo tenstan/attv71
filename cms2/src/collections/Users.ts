7abab787243d7bb74b54977461d53d7a22471373
Stan Martens <31350208+tenstan@users.noreply.github.com>
2025-02-28 16:45:21 +0100

    Add afterLogout hook to CMS for signing out with Azure Static Web Apps

--- cms2/src/collections/Users.ts
+++ cms2/src/collections/Users.ts
@@ -1,7 +1,12 @@
 import { User } from 'src/payload-types'
 import { createRoleField } from '../access/roles'
 import { isAdmin, isLoggedIn } from '../access/validation'
-import type { CollectionBeforeChangeHook, CollectionConfig } from 'payload'
+import {
+  generateExpiredPayloadCookie,
+  type CollectionAfterLogoutHook,
+  type CollectionBeforeChangeHook,
+  type CollectionConfig,
+} from 'payload'
 
 const setAdminRoleForFirstEverUserHook: CollectionBeforeChangeHook<User> = async ({
   req,
@@ -30,6 +35,30 @@ const setAdminRoleForFirstEverUserHook: CollectionBeforeChangeHook<User> = async
   return data
 }
 
+// This hook is a hack to make logout from the Admin UI work when this application is deployed to Azure Static Web Apps.
+//
+// Payload normally makes logout work by expiring the `payload-token` cookie with a Set-Cookie response header.
+// The cookie immediately expires when the browser receives the header, because it has an expiry date of Date.now() - 1000.
+//
+// Unfortunately, Azure Static Web Apps won't return Set-Cookie response headers with an expiration date before Date.now(),
+// presumably due to a bug (see: https://github.com/Azure/static-web-apps/issues/1214).
+// As mentioned in the link, a cookie can instead use Max-Age=0 to immediately expire the cookie,
+// which is what this handler applies to the `payload-token` cookie.
+const expirePayloadCookieHook: CollectionAfterLogoutHook = ({ req, collection }) => {
+  let expiredCookie = generateExpiredPayloadCookie({
+    collectionAuthConfig: collection.auth,
+    config: req.payload.config,
+    cookiePrefix: req.payload.config.cookiePrefix,
+  })
+
+  expiredCookie = expiredCookie.replace(/Expires=[^;]+/i, 'Max-Age=0')
+
+  req.responseHeaders = req.responseHeaders ?? new Headers()
+  req.responseHeaders.set('Set-Cookie', expiredCookie)
+
+  return req
+}
+
 export const Users: CollectionConfig & { slug: 'users' } = {
   slug: 'users',
   auth: {
@@ -47,6 +76,7 @@ export const Users: CollectionConfig & { slug: 'users' } = {
   },
   hooks: {
     beforeChange: [setAdminRoleForFirstEverUserHook],
+    afterLogout: [expirePayloadCookieHook],
   },
   fields: [createRoleField()],
 }
